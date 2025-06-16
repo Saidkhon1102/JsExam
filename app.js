c600c91d57faa3e0ef8de03527a2bd24ccf8bd04
Saidxon <saidxon@gmail.com>
2025-06-16 17:07:39 +0500

    .

--- app.js
+++ app.js
@@ -107,3 +107,9 @@
 // console.log(nextStr); 
 
 // 10-masala
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const juftSonlar = arr.filter(num => num % 2 === 0);
+// const toqSonlar = arr.filter(num => num % 2 !== 0);
+
+// console.log("Juftlar: ", juftSonlar);  
+// console.log("Toqlar: ", toqSonlar); 
