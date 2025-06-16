2054e1b5b94086ea41a8a92e3fafd0c3d1968c4a
Saidxon <saidxon@gmail.com>
2025-06-16 17:11:05 +0500

    .

--- app.js
+++ app.js
@@ -69,7 +69,6 @@
 
 // const rightAnswers = { 1: "B", 2: "A", 3: "C", 4: "D", 5: "B", 6: "C", 7: "A", 8: "D", 9: "A", 10: "B" };
 // const myAnswers = { 1: "C", 2: "A", 3: "D", 4: "D", 5: "B", 6: "C", 7: "B", 8: "C", 9: "A", 10: "B" };
-
 // let right = 0;
 
 // for (let key in rightAnswers) {
